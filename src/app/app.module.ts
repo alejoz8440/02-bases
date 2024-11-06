bf7c6e984a2befe33807b41c43e324c1ae7b19aa
Alejandro Tamayo <alejoz8440@gmail.com>
2024-11-05 23:12:58 -0500

    Inicio modulo 5 - Creacion Modulo DBZ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './Counter/counter.module';
 import { AppRoutingModule } from './app-routing.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
+
 
 
 
@@ -19,7 +21,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
