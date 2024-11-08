71ca8278de22eec90fe0debb7356e32a52197180
Alejandro Tamayo <alejoz8440@gmail.com>
2024-11-07 22:30:54 -0500

    Fin seccion 5

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import {  Component, Input } from '@angular/core';
+import {  Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -8,9 +8,20 @@ import { Character } from '../../interfaces/character.interface';
 
 })
 export class ListComponent {
+
   @Input()
   public characterList: Character[]=[{
     name:'Trunks',
     power: 10
-  }]
+  }];
+
+  //onDelete = Index value : number event
+  @Output()
+  public onDelete: EventEmitter<string>=new EventEmitter();
+
+  onDeleteCharacter(id?:string):void{
+    //TODO: Emitir el ID del personaje
+    if(!id) return;
+    this.onDelete.emit(id);
+  }
  }
